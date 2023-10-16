e76e8f9aea489dad4198d49e6e54801b613b8bb0
kjhwert <kjhwert11@naver.com>
2023-10-16 15:56:06 +0900

    chore: React-Query 설치 및 provider 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import appRouter from "./pages";
+import queryClient from "./configs/queryClient";
 
 const App = () => {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
