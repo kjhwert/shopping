27d59a20de7307f322b008ca7663127a3f36316f
kjhwert <kjhwert11@naver.com>
2023-10-16 21:06:19 +0900

    feat: Navigation Bar에 active page underline 추가

--- src/pages/GNB/index.tsx
+++ src/pages/GNB/index.tsx
@@ -1,15 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import * as S from "./styles";
 
 const GNB = () => {
+  const location = useLocation();
+
+  const isActivePage = (pathname: string) => {
+    return location.pathname.includes(pathname);
+  };
+
   return (
     <S.NavigationBar>
       <S.NavList>
         <S.NavLink>
-          <S.Link to="/products">상품</S.Link>
+          <S.Link to="/products" isActive={isActivePage("products")}>
+            상품
+          </S.Link>
         </S.NavLink>
         <S.NavLink>
-          <S.Link to="/cart">장바구니</S.Link>
+          <S.Link to="/cart" isActive={isActivePage("cart")}>
+            장바구니
+          </S.Link>
         </S.NavLink>
       </S.NavList>
       <Outlet />
--- src/pages/GNB/styles.ts
+++ src/pages/GNB/styles.ts
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Link as RouteLink } from "react-router-dom";
+import { css } from "@emotion/react";
 
 export const NavigationBar = styled.nav``;
 
@@ -15,19 +16,27 @@ export const NavLink = styled.li`
   position: relative;
 `;
 
-export const Link = styled(RouteLink)`
+const linkUnderline = css`
+  position: absolute;
+  content: "";
+  height: 6px;
+  background-color: #000;
+  top: 36px;
+  left: 0;
+  width: 100%;
+`;
+
+export const Link = styled(RouteLink, {
+  shouldForwardProp: (props) => props !== "isActive",
+})<{ isActive: boolean }>`
   color: #000;
   font-weight: 600;
   font-size: 36px;
   text-decoration: none;
 
+  ${(props) => props.isActive && linkUnderline}
+
   &:hover::after {
-    position: absolute;
-    content: "";
-    height: 6px;
-    background-color: #000;
-    top: 36px;
-    left: 0;
-    width: 100%;
+    ${linkUnderline}
   }
 `;
