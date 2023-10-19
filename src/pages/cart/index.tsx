ca3260c36be3e73c37a7d86b1c19f32d305ce5a1
kjhwert <kjhwert11@naver.com>
2023-10-19 21:19:30 +0900

    feat: 장바구니에 상품 없을 때 상품 페이지로 돌아갈 수 있도록 안내 추가

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,8 +1,11 @@
 import useCart from "./hooks/useCart";
 import * as S from "./styles";
 import useCoupon from "./hooks/useCoupon";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
+  const navigate = useNavigate();
+
   const { availableCoupons, selectedCoupon, onCouponSelect } = useCoupon();
 
   const {
@@ -37,68 +40,83 @@ const Cart = () => {
     }
   };
 
+  if (carts.length === 0) {
+    return (
+      <S.Layout>
+        <S.EmptyCart>
+          <h1>장바구니에 담은 상품이 없습니다.</h1>
+          <S.MoveToShopButton onClick={() => navigate("/products")}>
+            CONTINUE SHOPPING
+          </S.MoveToShopButton>
+        </S.EmptyCart>
+      </S.Layout>
+    );
+  }
+
   return (
     <S.Layout>
-      <S.Table>
-        <thead>
-          <tr>
-            <S.HeaderColumnCheckbox>선택</S.HeaderColumnCheckbox>
-            <S.HeaderColumnProduct>상품정보</S.HeaderColumnProduct>
-            <S.HeaderColumn>수량</S.HeaderColumn>
-            <S.HeaderColumn>주문금액</S.HeaderColumn>
-          </tr>
-        </thead>
-        <tbody>
-          {carts.map((product, index) => (
-            <tr key={product.item_no}>
-              <td>
-                <S.BodyColumn>
-                  <input type="checkbox" />
-                </S.BodyColumn>
-              </td>
-              <td>
-                <S.BodyColumnProduct>
-                  <S.Image src={product.detail_image_url} alt="상품 이미지" />
-                  <div>{product.item_name}</div>
-                  <button onClick={() => onRemoveItem(index)}>remove</button>
-                </S.BodyColumnProduct>
-              </td>
-              <td>
-                <S.BodyColumn>
-                  <input
-                    type="number"
-                    value={product.count}
-                    min={1}
-                    onChange={(e) =>
-                      onUpdateItem(index, { count: Number(e.target.value) })
-                    }
-                  />
-                </S.BodyColumn>
-              </td>
-              <td>
-                <S.BodyColumnPrice>
-                  {(product.discountPrice * product.count).toLocaleString()}
-                </S.BodyColumnPrice>
-              </td>
+      <S.CartSection>
+        <S.Table>
+          <thead>
+            <tr>
+              <S.HeaderColumnCheckbox>선택</S.HeaderColumnCheckbox>
+              <S.HeaderColumnProduct>상품정보</S.HeaderColumnProduct>
+              <S.HeaderColumn>수량</S.HeaderColumn>
+              <S.HeaderColumn>주문금액</S.HeaderColumn>
             </tr>
-          ))}
-        </tbody>
-      </S.Table>
-      <S.Section>
-        <h1>쿠폰</h1>
-        <select value={selectedCoupon?.id} onChange={handleSelectCoupon}>
-          <option value="-1">선택 안함</option>
-          {availableCoupons.map((coupon) => (
-            <option key={`coupon-option-${coupon.id}`} value={coupon.id}>
-              {coupon.title}
-            </option>
-          ))}
-        </select>
-      </S.Section>
-      <S.Section>
-        <h1>총 결제금액</h1>
-        <p>{totalPrice.toLocaleString()}</p>
-      </S.Section>
+          </thead>
+          <tbody>
+            {carts.map((product, index) => (
+              <tr key={product.item_no}>
+                <td>
+                  <S.BodyColumn>
+                    <input type="checkbox" />
+                  </S.BodyColumn>
+                </td>
+                <td>
+                  <S.BodyColumnProduct>
+                    <S.Image src={product.detail_image_url} alt="상품 이미지" />
+                    <div>{product.item_name}</div>
+                    <button onClick={() => onRemoveItem(index)}>remove</button>
+                  </S.BodyColumnProduct>
+                </td>
+                <td>
+                  <S.BodyColumn>
+                    <input
+                      type="number"
+                      value={product.count}
+                      min={1}
+                      onChange={(e) =>
+                        onUpdateItem(index, { count: Number(e.target.value) })
+                      }
+                    />
+                  </S.BodyColumn>
+                </td>
+                <td>
+                  <S.BodyColumnPrice>
+                    {(product.discountPrice * product.count).toLocaleString()}
+                  </S.BodyColumnPrice>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </S.Table>
+        <S.Section>
+          <h1>쿠폰</h1>
+          <select value={selectedCoupon?.id} onChange={handleSelectCoupon}>
+            <option value="-1">선택 안함</option>
+            {availableCoupons.map((coupon) => (
+              <option key={`coupon-option-${coupon.id}`} value={coupon.id}>
+                {coupon.title}
+              </option>
+            ))}
+          </select>
+        </S.Section>
+        <S.Section>
+          <h1>총 결제금액</h1>
+          <p>{totalPrice.toLocaleString()}</p>
+        </S.Section>
+      </S.CartSection>
     </S.Layout>
   );
 };
--- src/pages/cart/styles.ts
+++ src/pages/cart/styles.ts
@@ -47,3 +47,32 @@ export const Section = styled.section`
   justify-content: center;
   flex-direction: column;
 `;
+
+export const CartSection = styled.section``;
+
+export const EmptyCart = styled.section`
+  margin-top: 100px;
+  padding: 60px 0;
+  gap: 40px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border-top: 5px solid #000;
+  border-bottom: 2px solid #000;
+`;
+
+export const MoveToShopButton = styled.button`
+  cursor: pointer;
+  padding: 20px 40px;
+  background-color: #ffffff;
+  color: #000;
+  font-weight: bold;
+  font-size: 20px;
+  border: 1px solid #000;
+
+  &:hover {
+    color: #ff4800;
+  }
+`;
